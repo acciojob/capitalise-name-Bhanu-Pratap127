ef5ab5f17910aa053c3358804aa2046d0a070f95
Bhanu-Pratap127 <bhanupartap1273@gmail.com>
2024-11-30 22:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let ip = document.getElementById("fname")
 
-ip.addEventListner("blur",()=>{
+ip.addEventListener("blur",()=>{
 	ip.value = ip.value.toUpperCase()
 })
